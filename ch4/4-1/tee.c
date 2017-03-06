efd2ee266acc0780f194b63c7f43866435eb88c3
justin <justinledford@gmail.com>
2017-03-05 21:58:47 -0600

    add append option

--- ch4/4-1/tee.c
+++ ch4/4-1/tee.c
@@ -12,17 +12,31 @@
 
 int main(int argc, char *argv[])
 {
-    int outputFd, openFlags;
+    int opt, append, outputFd, openFlags;
     ssize_t n;
     mode_t filePerms;
     char buf[BUFSIZE];
+    char usage[] = "%s [-a] FILE\n  -a\tappend to file\n";
 
-    if (argc != 2 || strcmp(argv[1], "--help") == 0)
-        usageErr("%s output-file", argv[1]);
+    if (argc < 2 || argc > 3)
+        usageErr(usage, argv[0]);
 
-    openFlags = O_WRONLY | O_CREAT | O_TRUNC;
+    append = 0;
+    while ((opt = getopt(argc, argv, ":a")) != -1) {
+        switch (opt) {
+            case 'a':
+                append = 1;
+                break;
+            case '?':
+                usageErr(usage, argv[0]);
+                break;
+            default: fatal("Unexpected case in switch()");
+        }
+    }
+
+    openFlags = O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC);
     filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-    outputFd = open(argv[1], openFlags, filePerms);
+    outputFd = open(argv[argc-1], openFlags, filePerms);
     if (outputFd == -1)
         errExit("error opening %s", argv[1]);
 
